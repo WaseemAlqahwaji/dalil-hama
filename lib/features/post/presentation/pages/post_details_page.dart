b413ec51e489873bc70346d0a890ff6ef2841fad
WaseemAlqahwaji <waseemalqahwaji123@gmail.com>
2025-12-29 04:27:24 +0300

    ----

--- lib/features/post/presentation/pages/post_details_page.dart
+++ lib/features/post/presentation/pages/post_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:core_package/core_package.dart';
 import 'package:dalil_hama/features/core/presentation/page/gradient_scaffold.dart';
 import 'package:dalil_hama/features/post/domain/entity/post.dart';
+import 'package:dalil_hama/features/post/presentation/widget/post_scheme_card.dart';
 import 'package:flutter/material.dart';
 
 class PostDetailsPage extends StatefulWidget {
@@ -16,22 +17,49 @@ class _PostDetailsPageState extends State<PostDetailsPage> {
   @override
   Widget build(BuildContext context) {
     return GradientScaffold(
-      appBar: AppBar(
-        title: Text(widget.post.title),
-      ),
+      appBar: AppBar(title: Text(widget.post.title)),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
-          child: Column(
-            children: [
-              ImageWidget(
-                url: "fuck salem",
-                borderRadius: BorderRadius.circular(16),
-                width: double.infinity,
-                height: 250,
-              )
-            ],
-          ),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  16.height(),
+                  ImageWidget(
+                    url: "fuck salem",
+                    borderRadius: BorderRadius.circular(16),
+                    width: double.infinity,
+                    height: 250,
+                  ),
+                  16.height(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          widget.post.title,
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      ),
+                      Icon(Icons.abc),
+                    ],
+                  ),
+                  16.height(),
+                  ListView.separated(
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) => PostSchemeCard(
+                      schemaAttribute: widget.post.attributes[index],
+                    ),
+                    separatorBuilder: (context, index) => 8.height(),
+                    itemCount: widget.post.attributes.length,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/features/post/presentation/widget/post_scheme_card.dart
@@ -0,0 +1,35 @@
+import 'package:core_package/core_package.dart';
+import 'package:dalil_hama/features/schema/domain/entity/schema_attribute.dart';
+import 'package:flutter/material.dart';
+
+class PostSchemeCard extends StatelessWidget {
+  final SchemaAttribute schemaAttribute;
+  const PostSchemeCard({super.key, required this.schemaAttribute});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomCardWidget(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    schemaAttribute.title,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  4.height(),
+                  Text(schemaAttribute.value.toString()),
+                ],
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
